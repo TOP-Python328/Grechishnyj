431d41c924139bc052c0988fdd65953a2dab9293
Pavel-Grechishny <grechishny.p@mail.ru>
2023-07-10 18:59:32 +0300

    Added solution version 0

---
+++ 2023.07.07/1.py
@@ -0,0 +1,10 @@
+name = input('Введите имя: ')
+lastname = input('Введите фамилию: ')
+year = int(input('Введите год рождения: '))
+
+print(lastname + ' ' + name, 2023 - year, sep=', ')
+
+# Введите имя: Pavel
+# Введите фамилию: Grechishny
+# Введите год рождения: 1986
+# Grechishny Pavel, 37

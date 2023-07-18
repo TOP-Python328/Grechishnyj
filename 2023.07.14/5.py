460aac16986696337cb26738f6390baafde5936a
Pavel-Grechishny <grechishny.p@mail.ru>
2023-07-18 11:45:34 +0300

    опубликовано: решение 5.py

---
+++ 2023.07.14/5.py
@@ -0,0 +1,19 @@
+cell1, cell2 = input(), input()
+
+print('да') if cell1[0] == cell2[0] or cell1[1] == cell2[1] else print('нет')
+
+# c4
+# d4
+# да
+
+# a2
+# c4
+# нет
+
+# h8
+# a8
+# да
+
+# d4
+# e5
+# нет

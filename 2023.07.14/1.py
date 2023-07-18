676de16062e2120991d43d42b2d10ce8ffff48a7
Pavel-Grechishny <grechishny.p@mail.ru>
2023-07-18 09:31:14 +0300

    опубликовано: решение 1.py

---
+++ 2023.07.14/1.py
@@ -0,0 +1,15 @@
+    
+num_1 = float(input('Число 1: '))
+num_2 = float(input('Число 2: '))
+num_3 = float(input('Число 3: '))
+
+total = num_1 if num_1 > 0 else 0
+total += num_2 if num_2 > 0 else 0
+total += num_3 if num_3 > 0 else 0
+
+print(f'Сумма: {total}')
+
+# Число 1: 4
+# Число 2: -22
+# Число 3: 1.5
+# Сумма: 5.5

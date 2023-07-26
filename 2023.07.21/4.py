6084468b2f9fa4f71568236254d3a28696f5f7d8
Pavel-Grechishny <grechishny.p@mail.ru>
2023-07-26 17:28:21 +0300

    опубликовано: предварительное решение 4.py

---
+++ 2023.07.21/4.py
@@ -0,0 +1,24 @@
+num_first = int('1' + '0' * (int(input('Введите разрядность: ')) - 1))
+numbers = list(i for i in range(num_first, num_first * 10))
+digits_simple = []
+cnt = 0
+
+for num in numbers:
+    for i in range(1, num + 1):
+        if not num % i:
+            cnt += 1         
+    if cnt == 2:
+        digits_simple.append(num)
+    cnt = 0
+
+print(f'Простых чисел: {len(digits_simple)}')
+        
+        
+# Введите разрядность: 1
+# Простых чисел: 4
+
+# Введите разрядность: 2
+# Простых чисел: 21
+
+# Введите разрядность: 3
+# Простых чисел: 143   

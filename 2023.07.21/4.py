1bb1b7fa5d2fbb0114e52a0151c0bcecca4c3ce7
Pavel-Grechishny <grechishny.p@mail.ru>
2023-07-26 17:51:16 +0300

    изменено: место объявления переменной cnt

--- 2023.07.21/4.py
+++ 2023.07.21/4.py
@@ -1,15 +1,15 @@
 num_first = int('1' + '0' * (int(input('Введите разрядность: ')) - 1))
 numbers = list(i for i in range(num_first, num_first * 10))
 digits_simple = []
-cnt = 0
+
 
 for num in numbers:
+    cnt = 0
     for i in range(1, num + 1):
         if not num % i:
             cnt += 1         
     if cnt == 2:
         digits_simple.append(num)
-    cnt = 0
 
 print(f'Простых чисел: {len(digits_simple)}')
         

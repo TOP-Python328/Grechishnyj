cccf45d8f75c9e84a1a11c0416ce85d6c2313f1e
Pavel-Grechishny <grechishny.p@mail.ru>
2023-07-27 22:58:09 +0300

    опубликовано: предварительное решение 8.py

---
+++ 2023.07.21/8.py
@@ -0,0 +1,21 @@
+numbers = int(input('stdin: '))
+
+num_1, num_2 = 1, 1
+
+sequence = list()
+
+for _ in range(numbers):
+    sequence.append(num_1)
+    num_1, num_2 = num_2, num_1 + num_2
+    
+print('stdout: ', end='')
+print(*sequence)
+    
+# stdin: 1
+# stdout: 1
+
+# stdin: 7
+# stdout: 1 1 2 3 5 8 13
+
+# stdin: 17
+# stdout: 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597

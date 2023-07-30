471221f3eef7a59bd4a73bfd1d42fd4c270e8fdd
Pavel-Grechishny <grechishny.p@mail.ru>
2023-07-30 05:20:27 +0300

    удалено: round()

--- 2023.07.21/5.py
+++ 2023.07.21/5.py
@@ -1,7 +1,7 @@
 text = input('Текст телеграммы: ')
 chars = [char for char in text if char != ' ']
 
-price_msg = str(round(len(chars) * 30))
+price_msg = str(len(chars) * 30)
 
 print(f'{price_msg[:-2] or 0} руб. {price_msg[-2:]} коп.')
 
@@ -12,5 +12,5 @@ print(f'{price_msg[:-2] or 0} руб. {price_msg[-2:]} коп.')
 # Текст телеграммы: 1 2            3
 # 0 руб. 90 коп.
 
-# Текст телеграммы: грузите апельсины бочках братья карамазовы
+# Текст телеграммы: грузите апельсины в бочках братья карамазовы
 # 11 руб. 40 коп.

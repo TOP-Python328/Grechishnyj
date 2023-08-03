f366e7ee666e23b147b4002e3f0cb3eb429ec93e
Pavel-Grechishny <grechishny.p@mail.ru>
2023-08-03 14:31:24 +0300

    опубликовано: предварительное решение 6.py

---
+++ 2023.07.28/6.py
@@ -0,0 +1,30 @@
+string = input() 
+
+true_set = not len(set(string) - {'b', '0', '1'})
+true_prefix = string[:2] == '0b' or string[:1] == 'b' or 'b' not in string
+
+message = 'да' if true_set and true_prefix else 'нет'
+
+print(message)
+
+
+# 10111
+# да
+
+# b1011101
+# да
+
+# 0b1011101
+# да
+
+# 10222
+# нет
+
+# b10222
+# нет
+
+# 1b10111
+# нет
+
+# 2b101011
+# нет

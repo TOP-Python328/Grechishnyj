e1e57eff1459b47dd314ea39a772e6705624548a
Pavel-Grechishny <grechishny.p@mail.ru>
2023-08-03 12:28:03 +0300

    заменено: index() на ord()

--- 2023.07.14/6.py
+++ 2023.07.14/6.py
@@ -1,11 +1,10 @@
 cell1, cell2 = input(), input()
-horz, vert = 'abcdefgh', '12345678'
-# ИСПРАВИТЬ: для первого символа можно применить встроенную функцию ord() — она тоже выполнится быстрее, чем метод index()
-x1, y1 = horz.index(cell1[0]), vert.index(cell1[1])
-x2, y2 = horz.index(cell2[0]), vert.index(cell2[1])
+
+# horz, vert = 'abcdefgh', '12345678'# ИСПРАВИТЬ: для первого символа можно применить встроенную функцию ord() — она тоже выполнится быстрее, чем метод index()
+x1, y1 = ord(cell1[0]), int(cell1[1])
+x2, y2 = ord(cell2[0]), int(cell2[1])
 
 print('да') if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1 else print('нет')
-
 
 # g3
 # f2
@@ -25,3 +24,25 @@ print('да') if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1 else print('нет')
 
 
 # ИТОГ: отлично — 4/5
+
+# ТЕСТЫ ПОСЛЕ ИСПРАВЛЕНИЙ
+
+# g3
+# f2
+# да
+
+# g3
+# f2
+# да
+
+# c6
+# d4
+# нет
+
+# a1
+# b2
+# да
+
+# h8
+# h6
+# нет

4d0c2b8a58a1c67aa34c21162bb3644bd0585dac
Pavel-Grechishny <grechishny.p@mail.ru>
2023-08-08 15:14:22 +0300

    внесено: правки по замечанниям 1.py

--- 2023.07.28/1.py
+++ 2023.07.28/1.py
@@ -2,12 +2,12 @@ email = input('Введите e-mail: ')
 correct = 'нет'
 
 if '.' in email and '@' in email:
+    i_dot, i_at = email.rindex('.'), email.rindex('@')
     # ИСПРАВИТЬ: операции со строками являются довольно ресурсозатратными, поэтому в общем случае их выполняют единожды, запоминая результат — оптимизируйте вызовы rindex('.')
-    if email.rindex('.') - email.rindex('@') > 1 and email.rindex('.') < len(email) - 1:
+    if i_dot - i_at > 1 and i_dot < len(email) - 1:
         correct = 'да'
 
 print(f'Корректный e-mail: {correct}')
-
 
 # Введите e-mail: grechishny.p@mail.ru
 # Корректный e-mail: да

5684aa20cb51212927843618e82ef501d7007d60
Pavel Grechishny <grechishny.p@mail.ru>
2023-08-30 15:22:27 +0300

    опубликовано: алгоритм 2.py

--- 2023.08.25/2.py
+++ 2023.08.25/2.py
@@ -1,9 +1,9 @@
 def deck() -> 'generator':
     """Функция создает упорядоченную колоду игральных карт"""
     
-    nominals = tuple(i for i in range(2, 15))
+    nominals = tuple(num for num in range(2, 15))
     cards_deck = ('черви', 'бубны', 'пики', 'трефы')
     
-    for i in cards_deck:
-        for j in nominals:
-            yield (j, i)
+    for card_deck in cards_deck:
+        for nominal in nominals:
+            yield (nominal, card_deck)

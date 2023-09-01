19ce146896f7c8393bf4b64f42ba5362cbbc9df7
Pavel Grechishny <grechishny.p@mail.ru>
2023-09-01 14:02:23 +0300

    опубликовано: 2.py - 1.0.0

--- 2023.08.25/2.py
+++ 2023.08.25/2.py
@@ -7,3 +7,19 @@ def deck() -> 'generator':
     for card_deck in cards_deck:
         for nominal in nominals:
             yield (nominal, card_deck)
+            
+# >>> cards = deck()
+# >>> type(cards)
+# <class 'generator'>
+# >>> cards.__next__()
+# (2, 'черви')
+# >>> cards.__next__()
+# (3, 'черви')
+# >>> cards.__next__()
+# (4, 'черви')
+# >>> cards.__next__()
+# (5, 'черви')
+
+# >>> cards = deck()
+# >>> list(cards)[::13]
+# [(2, 'черви'), (2, 'бубны'), (2, 'пики'), (2, 'трефы')]

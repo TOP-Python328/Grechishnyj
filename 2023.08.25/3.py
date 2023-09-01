22f88ecedfcdbf68cc3d69d4fa3a902990e33066
Pavel Grechishny <grechishny.p@mail.ru>
2023-09-01 14:25:51 +0300

    опубликовано: 3.py - 1.0.0

--- 2023.08.25/3.py
+++ 2023.08.25/3.py
@@ -2,8 +2,8 @@ def math_function_resolver(
         math_func: 'function', 
         /, 
         x_main: int | float, 
-        *x_args: int | float,
-        *,
+        *x_args: int | float, 
+        # *, - получаем ошибку SyntaxError: * argument may appear only once
         strings: bool=False
     ) -> list[float | str]:
     """
@@ -24,4 +24,13 @@ def math_function_resolver(
         res = round(math_func(x), 2)
         results.append(str(res) if strings else res)
     
-    return results   
+    return results 
+
+# >>> math_function_resolver(lambda x: 0.5*x + 2, *range(1, 10))
+# [2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5]
+
+# >>> math_function_resolver(lambda x: -0.5*x + 2, *range(1, 10))
+# [1.5, 1.0, 0.5, 0.0, -0.5, -1.0, -1.5, -2.0, -2.5]
+
+# >>> math_function_resolver(lambda x: 2.72**x, *range(1, 10), strings=True)
+# ['2.72', '7.4', '20.12', '54.74', '148.88', '404.96', '1101.49', '2996.07', '8149.3']    

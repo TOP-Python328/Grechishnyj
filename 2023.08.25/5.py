39f085efbc27b64b617437f639d47197b5e838b6
Pavel Grechishny <grechishny.p@mail.ru>
2023-09-01 13:47:34 +0300

    опубликовано: 5.py - 1.0.0

--- 2023.08.25/5.py
+++ 2023.08.25/5.py
@@ -3,56 +3,46 @@ def logger(func_obj: 'function') -> 'function':
     
     def wrapper(*args, **kwargs):
         """Функция-обертка, вызывает декорируемую функцию"""
-        
-        result = func_obj(*args, **kwargs)
-        
         kwargs_default = dict(zip(reversed(func_obj.__code__.co_varnames),reversed(func_obj.__defaults__)))
         kwargs_update = kwargs_default | dict(reversed(tuple(kwargs.items())))
 
         args_position = tuple(map(lambda item: str(item), args))
         args_keywords = tuple(map(lambda item: f'{item[0]}={item[1]}', reversed(kwargs_update.items())))
+            
+        string_params = ', '.join((*args_position, *args_keywords))
+        string_logger = f'{func_obj.__name__}({string_params})'
         
-        str_params = ', '.join((*args_position, *args_keywords))    
-        log_string = f'{func_obj.__name__}({str_params}) -> {result}'
-        print(log_string)
-        return result
+        try: 
+            result = func_obj(*args, **kwargs)
+            print(f'{string_logger} -> {result}')
+            return result 
+            
+        except Exception as exception:
+            return f'{string_logger} .. {exception.__class__.__name__}: {exception}'
            
     return wrapper
     
+
+# >>> def div_round(num1, num2, digits=0):
+# ...     return round(num1 / num2, digits)
+# ...
+
+# >>> div_round(1, 3, digits=2)
+# 0.33
+
+# >>> div_round(7, 2)
+# 4.0
+
+# >>> div_round = logger(div_round)
+
+# >>> div_round(7, 2)
+# div_round(7, 2, digits=0) -> 4.0
+# 4.0
+
+# >>> div_round(7, 0)
+# 'div_round(7, 0, digits=0) .. ZeroDivisionError: division by zero'
+
     
-# def test_func(n1: int, s2: int, n3: int, key: bool=False, key2: bool=False):
-#     return round(n1 / s2, 3)
-    
-# print(test_func(55, 33, 44))
+  
+   
 
-# test_func = logger(test_func)
-# print(test_func(55, 21, 44))
-# print(test_func(55, 21, 44, key=2))
-# print(test_func(55, 21, 44, key=2, key2=2))
-# print(test_func(55, 21, n3=44, key=2, key2=2))
-# print(test_func(55, s2=21, n3=44, key=2, key2=2))
-# print(test_func(n1=55, s2=21, n3=44, key=2, key2=2))
-
-# >>> test_func(55, 21, 44)
-# 2.619
-
-# test_func = logger(test_func)
-
-# >>> test_func(55, 21, 44)
-# test_func(55, 21, 44, key=False, key2=False) -> 2.619
-# 2.619
-# >>> test_func(55, 21, 44, key=2)
-# test_func(55, 21, 44, key=2, key2=False) -> 2.619
-# 2.619
-# >>> test_func(55, 21, 44, key=2, key2=2)
-# test_func(55, 21, 44, key=2, key2=2) -> 2.619
-# 2.619
-# >>> test_func(55, 21, n3=44, key=2, key2=2)
-# test_func(55, 21, n3=44, key=2, key2=2) -> 2.619
-# 2.619
-# >>> test_func(55, s2=21, n3=44, key=2, key2=2)
-# test_func(55, s2=21, n3=44, key=2, key2=2) -> 2.619
-# 2.619
-# >>> test_func(n1=55, s2=21, n3=44, key=2, key2=2)
-# test_func(n1=55, s2=21, n3=44, key=2, key2=2) -> 2.619
-# 2.619

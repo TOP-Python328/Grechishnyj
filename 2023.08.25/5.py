67f4bfccd8a23af412da03ade6e1094fc7ae5698
Pavel Grechishny <grechishny.p@mail.ru>
2023-09-08 12:56:59 +0300

    обновлено: функция logger()

--- 2023.08.25/5.py
+++ 2023.08.25/5.py
@@ -4,15 +4,20 @@ def logger(func_obj: 'function') -> 'function':
     def wrapper(*args, **kwargs):
         """Функция-обёртка, вызывает декорируемую функцию."""
 
-        # УДАЛЕНО: вычисление переменной kwargs_default
-        # kwargs_default = dict(zip(
-            # reversed(func_obj.__code__.co_varnames),
-            # ИСПРАВИТЬ: у объекта функции есть два атрибута __defaults__ и __kwdefaults__ — если мы объявляем строго ключевые параметры, то их значения по умолчанию попадают только в __kwdefaults__; а если при этом в строго позиционных и позиционно-ключевых параметрах значений пол умолчанию нет, то в атрибуте __defaults__ будет записан объект None
-            #reversed(func_obj.__kwdefaults__)
-        # ))
+        # ИСПРАВИТЬ: у объекта функции есть два атрибута __defaults__ и __kwdefaults__ — если мы объявляем строго ключевые параметры, то их значения по умолчанию попадают только в __kwdefaults__; а если при этом в строго позиционных и позиционно-ключевых параметрах значений пол умолчанию нет, то в атрибуте __defaults__ будет записан объект None
+        
+        kwargs_default = dict()
+        if func_obj.__kwdefaults__:
+            kwargs_default = dict(func_obj.__kwdefaults__) | dict(kwargs.items())
+            
+        if func_obj.__defaults__:
+            kwargs_default = dict(zip(
+                reversed(func_obj.__code__.co_varnames),
+                reversed(func_obj.__defaults__)
+            ))
         
         # ИСПОЛЬЗОВАТЬ: объект dict_items может быть передан в встроенную функцию reversed() без преобразования
-        kwargs_update = func_obj.__kwdefaults__ | dict(kwargs.items())
+        kwargs_update = kwargs_default | dict(kwargs.items())
 
         # ИСПОЛЬЗОВАТЬ: lambda функция избыточна
         args_position = tuple(map(str, args))
@@ -40,17 +45,31 @@ def logger(func_obj: 'function') -> 'function':
 # ...     return round(num1 / num2, digits)
 # ...
 # >>> div_round = logger(div_round)
-# >>> div_round(7, 6, digits=5)
-# div_round(7, 6, digits=5, key=False) -> 1.16667
-# 1.16667
-# >>> div_round(7, 6, digits=1, key=True)
-# div_round(7, 6, digits=1, key=True) -> 1.2
-# 1.2
+# >>> div_round(7, 6, digits=3)
+# div_round(7, 6, digits=3, key=False) -> 1.167
+# 1.167
 # >>> div_round(7, 6)
 # div_round(7, 6, digits=0, key=False) -> 1.0
 # 1.0
+
+# >>> def div_round(num1, num2, digits=0, key=False):
+# ...     return round(num1 / num2, digits)
+# ...
+# >>> div_round = logger(div_round)
+# >>> div_round(7, 6, digits=3)
+# div_round(7, 6, key=False, digits=3) -> 1.167
+# 1.167
 # >>> div_round(7, 0)
-# div_round(7, 0, digits=0, key=False) .. ZeroDivisionError: division by zero
+# div_round(7, 0, key=False, digits=0) .. ZeroDivisionError: division by zero
 # >>> div_round(7)
-# div_round(7, digits=0, key=False) .. TypeError: div_round() missing 1 required positional argument: 'num2'
-# >>>
+# div_round(7, key=False, digits=0) .. TypeError: div_round() missing 1 required positional argument: 'num2'
+
+# >>> def div_round(num1, num2):
+# ...     return round(num1 / num2)
+# ...
+# >>> div_round = logger(div_round)
+# >>> div_round(7, 6)
+# div_round(7, 6) -> 1
+# 1
+# >>> div_round(7, 's')
+# div_round(7, s) .. TypeError: unsupported operand type(s) for /: 'int' and 'str'
--- 2023.09.03/5.py
+++ 2023.09.03/5.py
@@ -3,55 +3,53 @@ from sys import path
 from datetime import datetime as dt
 
 def logger(func_obj: 'function') -> 'function':
-    """
-        Функция - декоратор, ведет журнал вызовов декорируемой функции
-        Вызовы функции записываются в файл - журнал вызовов (data/function_calls.log)
+    """Функция - декоратор, ведет журнал вызовов декорируемой функции.
+    
+    Вызовы функции записываются в файл - журнал вызовов (data/function_calls.log).
     """
     
     def wrapper(*args, **kwargs):
-        """Функция-обертка, вызывает декорируемую функцию"""
-        
-        kwargs_default = dict(zip(reversed(func_obj.__code__.co_varnames),reversed(func_obj.__defaults__)))
-        kwargs_update = kwargs_default | dict(reversed(tuple(kwargs.items())))
-
-        args_position = tuple(map(lambda item: str(item), args))
-        args_keywords = tuple(map(lambda item: f'{item[0]}={item[1]}', reversed(kwargs_update.items())))
-            
+        """Функция-обёртка, вызывает декорируемую функцию."""
+        kwargs_default = dict()
+        if func_obj.__kwdefaults__:
+            kwargs_default = dict(func_obj.__kwdefaults__) | dict(kwargs.items())
+        if func_obj.__defaults__:
+            kwargs_default = dict(zip(
+                reversed(func_obj.__code__.co_varnames),
+                reversed(func_obj.__defaults__)
+            ))
+        kwargs_update = kwargs_default | dict(kwargs.items())
+        args_position = tuple(map(str, args))
+        args_keywords = tuple(map(
+            lambda item: f'{item[0]}={item[1]}',
+            kwargs_update.items()
+        )) 
         string_params = ', '.join((*args_position, *args_keywords))
         string_logger = f'{func_obj.__name__}({string_params})'
-        
         try: 
             result = func_obj(*args, **kwargs)
             logstring = (f'{dt.now().strftime("%Y.%m.%d %H:%M:%S")} -> {string_logger} -> {result}')
-            
         except Exception as exception:
             result = None
             logstring = f'{dt.now().strftime("%Y.%m.%d %H:%M:%S")} -> {string_logger} .. {exception.__class__.__name__}: {exception}'
-            
         file = Path(path[0]) / 'data/function_calls.log'
         with open(file, 'a', encoding='utf-8') as file_content:
             print(logstring, file=file_content)
-        
         return result
-        
     return wrapper
 
-# Декоратор logger работает формирует строку параметров не верно!!!
-
 # Результаты вызова  
-# >>> def div_round(num1, num2, digits=0):
+# >>> def div_round(num1, num2, *, digits=0):
 # ...     return round(num1 / num2, digits)
 # ...
 # >>> div_round = logger(div_round)
-# >>> div_round(5, 2)
-# 2.0
-# >>> div_round(5, 2, 1)
-# 2.5
-# >>> div_round(5, 0)
-# >>> div_round(5)
+# >>> div_round(7, 6)
+# 1.0
+# >>> div_round(7, 6, digits=3)
+# 1.167
+# >>> div_round(7, 0)
 
 # Записи журнала function_calls.log
-# 2023.09.07 11:18:20 -> div_round(5, 2, digits=0) -> 2.0
-# 2023.09.07 11:18:33 -> div_round(5, 2, 1, digits=0) -> 2.5
-# 2023.09.07 11:20:23 -> div_round(5, 0, digits=0) .. ZeroDivisionError: division by zero
-# 2023.09.07 11:20:29 -> div_round(5, digits=0) .. TypeError: div_round() missing 1 required positional argument: 'num2'
+# 2023.09.08 12:52:18 -> div_round(7, 6, digits=0) -> 1.0
+# 2023.09.08 12:52:34 -> div_round(7, 6, digits=3) -> 1.167
+# 2023.09.08 12:52:59 -> div_round(7, 0, digits=0) .. ZeroDivisionError: division by zero

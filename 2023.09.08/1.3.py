1272e92b8bf4bc0af06ae4f42ecce353b3ab06e3
Pavel Grechishny <grechishny.p@mail.ru>
2023-10-07 12:16:19 +0300

    опубликовано: решение 1.3.py

--- 2023.09.08/1.3.py
+++ 2023.09.08/1.3.py
@@ -1,21 +1,19 @@
 from pathlib import Path
 from sys import path
-from random import choice, uniform
+from random import choice, randrange
 from typing import Literal
 from datetime import date
 
 
-
-# print(typing.Literal['мужской', 'женский'])
-
-
 # Файлы хранения данных
 ROOT_DIR = Path(path[0])
-FIRST_NAMES = ROOT_DIR / 'first_names.txt'
-LAST_NAMES = ROOT_DIR / 'last_names.txt'
+NAMES = ROOT_DIR / 'names.txt'
+PATRONYMICS = ROOT_DIR / 'patronymics.txt'
+SURNAMES = ROOT_DIR / 'surnames.txt'
 
 # Перменные для аннотаций
-Names = dict[str, list[str]]
+DictGenders = dict[str, [list[str]]]
+Names = dict[DictGenders]
 Name = str
 Patronymic = str
 Surname = str
@@ -25,188 +23,115 @@ Mobile = str
 Person = dict[Name, Patronymic, Surname, Gender, Birth, Mobile]
 
 # Глобальное пространство имён
-names: Names = {}
+names_db: Names = {}
+TOKENS = ('мужской', 'женский')
+
+# Структура файлов
+# мужской:Иван!Перт!Алексей...\nженский:Ольга!Наталья!...!Яна
+
+
+def generate_gender(data_file_text: str) -> DictGenders:
+    """Вспомогательная функция генерирует словарь с ключами мужской или женский"""
+    data_file_text = data_file_text.strip().split('\n')
+    dict_gender: DictGenders = {}
+    for text_line in data_file_text:
+        text_line = text_line.split(':')
+        dict_gender[text_line[0]] = text_line[1].split('!')
+    return dict_gender
+
+    
+def get_rand_date() -> Birth:
+    """Вспомогательная функция генерации даты"""
+    large_month: list[int] = [1, 3, 5, 7, 8, 10, 12]
+    small_montn: list[int] = [4, 6, 9, 11]
+    year: int = randrange(1920, 2020)
+    month: int = randrange(1, 13)
+    day: int = None
+    if month == 2:
+        day = randrange(1, 30) if year // 4 == 0 else randrange(1, 29)
+    elif month in large_month:
+        day = randrange(1, 32)
+    elif month in small_montn:
+        day = randrange(1, 31)
+    return date(year, month, day)
+
+    
+def get_mobile() -> Mobile:
+    """Вспомогательная функция генерации номера телефона"""
+    mobile: Mobile = '+79'
+    for _ in range(9):
+        mobile += str(randrange(0, 10))
+    return mobile
 
 
 def load_data() -> None:
-    """Функция создания словаря имен из файлов данных"""
-    
-    first_names = FIRST_NAMES.read_text(encoding='utf-8')
-    last_names = LAST_NAMES.read_text(encoding='utf-8')
-    
-    first_names = first_names.strip().split('\n')
-    last_names = last_names.strip().split('!')
-    
-    names['мужской'] = first_names[1].split('!')
-    names['женский'] = first_names[0].split('!')
-    names['фамилия'] = last_names
-    return None
-    
-
-
-    
+    """Функция создания словаря из файлов данных"""
+    names: DictGenders = NAMES.read_text(encoding='utf-8')
+    patronymics: DictGenders = PATRONYMICS.read_text(encoding='utf-8')
+    surnames: DictGenders = SURNAMES.read_text(encoding='utf-8')  
+    names_db['имена'] = generate_gender(names)
+    names_db['отчества'] = generate_gender(patronymics)
+    names_db['фамилии'] = generate_gender(surnames)
+ 
     
 def generate_person() -> Person:
-    """Вспомогательная функция генерирует анкету человека со случаными данными"""
+    """Функция генерирует анкету человека со случаными данными"""
     person: Person = {}
-    pointer = choice(('мужской', 'женский'))
-    person['имя']: Name = choice(names[pointer])
-    
-    def get_patronymic(pointer) -> Patronymic:
-        """Функция преобразует мужское имя в отчество"""
-        patronymic = choice(names['мужской'])
-        start = patronymic[:-2]
-        end = patronymic[-2:]
-        if pointer == 'женский':
-            if end == 'ей':
-                end = 'евна'
-            elif end == 'ий':
-                end = 'ьевна'
-            elif end == 'ел':
-                end = 'ловна'
-            else:
-                end = 'овна'
-                return f'{patronymic}{end}'
-            return f'{start}{end}'
-        else:
-            if end == 'ей':
-                end = 'евич'
-            elif end == 'ий':
-                end = 'ьевич'
-            elif end == 'ел':
-                end = 'лович'
-            else:
-                end = 'ович'
-                return f'{patronymic}{end}'
-            return f'{start}{end}'
-            
-    def get_surname(pointer) -> Surname:
-        """Вспомогательная функция возвращает женскую фамилию"""
-        last_name = choice(names['фамилия'])
-        start = last_name[:-2]
-        end = last_name[-2:]
-        if pointer == 'женский':
-            if end in ('ий', 'ый'):
-                end = 'ая'
-                return f'{start}{end}'
-            else:
-                end = 'а'
-                return f'{last_name}{end}'
-            return f'{start}{end}'
-        else:
-            return last_name
-    
-    
-    def get_rand_date() -> Birth:
-        """Вспомогательная функция генерации даты"""
-        lg_month = [1, 3, 5, 7, 8, 10, 12]
-        sm_montn = [4, 6, 9, 11]
-        year = int(uniform(1925, 2025))
-        month = int(uniform(1, 13))
-        if month == 2:
-            day = uniform(1, 30) if year // 4 == 0 else int(uniform(1, 29))
-        elif month in lg_month:
-            day = uniform(1, 32)
-        elif month in sm_montn:
-            day = uniform(1, 31)
-        day = int(day)    
-        return date(year, month, day)
-        
-    def get_mobile() -> Mobile:
-        """Вспомогательная функция генерации номера телефона"""
-        mobile = '+79'
-        for _ in range(9):
-            mobile += str(int(uniform(0, 10)))
-        return mobile
-        
-    person['отчество']: Patronymic = get_patronymic(pointer)
-    person['фамилия']: Surname = get_surname(pointer)
+    pointer: str = choice(TOKENS)
+    person['имя']: Name = choice(names_db['имена'][pointer])
+    person['отчество']: Patronymic = choice(names_db['отчества'][pointer])
+    person['фамилия']: Surname = choice(names_db['фамилии'][pointer])
     person['пол']: Gender = pointer
     person['дата рождения']: Birth = get_rand_date()
     person['мобильный']: Mobile = get_mobile()
-    
     return person
-    
 
-# if __name__ == '__main__':
-    # load_data()
-    # generate_person()
-    
-    
 
- # 17:29:11 > python -i 2023.09.08\1.3.py
+# 12:00:24 > python -i 2023.09.08\1.3.py
 # >>> from pprint import pprint
-# >>>
 # >>> load_data()
-# >>>
-# >>> person = generate_person()
-# >>> pprint(person)
-# {'дата рождения': datetime.date(1946, 5, 23),
- # 'имя': 'Ростислав',
- # 'мобильный': '+79287029220',
- # 'отчество': 'Ахмедович',
+# >>> for _ in range(7):
+# ...     pprint(generate_person(), sort_dicts=False)
+# ...
+# {'имя': 'Клавдия',
+ # 'отчество': 'Леонидовна',
+ # 'фамилия': 'Швецова',
+ # 'пол': 'женский',
+ # 'дата рождения': datetime.date(2006, 11, 2),
+ # 'мобильный': '+79837320367'}
+# {'имя': 'Самуил',
+ # 'отчество': 'Аксёнович',
+ # 'фамилия': 'Корольков',
  # 'пол': 'мужской',
- # 'фамилия': 'Калугин'}
-# >>>
-# >>> person = generate_person()
-# >>> pprint(person)
-# {'дата рождения': datetime.date(1961, 2, 19),
- # 'имя': 'Феодосия',
- # 'мобильный': '+79164256676',
- # 'отчество': 'Филипповна',
+ # 'дата рождения': datetime.date(1952, 8, 18),
+ # 'мобильный': '+79294197577'}
+# {'имя': 'Алина',
+ # 'отчество': 'Павловна',
+ # 'фамилия': 'Гончарова',
  # 'пол': 'женский',
- # 'фамилия': 'Судакова'}
-# >>>
-# >>> person = generate_person()
-# >>> pprint(person)
-# {'дата рождения': datetime.date(2014, 8, 23),
- # 'имя': 'Панкрат',
- # 'мобильный': '+79210926755',
- # 'отчество': 'Романович',
+ # 'дата рождения': datetime.date(1948, 5, 18),
+ # 'мобильный': '+79378551140'}
+# {'имя': 'Леон',
+ # 'отчество': 'Каллистратович',
+ # 'фамилия': 'Гончаров',
  # 'пол': 'мужской',
- # 'фамилия': 'Федосов'}
-# >>>
-# >>> person = generate_person()
-# >>> pprint(person)
-# {'дата рождения': datetime.date(1971, 10, 8),
- # 'имя': 'Татьяна',
- # 'мобильный': '+79260236994',
- # 'отчество': 'Касьяновна',
+ # 'дата рождения': datetime.date(1932, 12, 2),
+ # 'мобильный': '+79203899507'}
+# {'имя': 'Иоанна',
+ # 'отчество': 'Никодимовна',
+ # 'фамилия': 'Яковлева',
  # 'пол': 'женский',
- # 'фамилия': 'Булгакова'}
-# >>>
-# >>> person = generate_person()
-# >>> pprint(person)
-# {'дата рождения': datetime.date(2001, 1, 4),
- # 'имя': 'Аарон',
- # 'мобильный': '+79237577279',
- # 'отчество': 'Азатович',
- # 'пол': 'мужской',
- # 'фамилия': 'Серов'}
-# >>>
-# >>> person = generate_person()
-# >>> pprint(person)
-# {'дата рождения': datetime.date(1972, 6, 23),
- # 'имя': 'Лилия',
- # 'мобильный': '+79130475955',
- # 'отчество': 'Августовна',
+ # 'дата рождения': datetime.date(1995, 5, 21),
+ # 'мобильный': '+79623107685'}
+# {'имя': 'Динара',
+ # 'отчество': 'Яковлевна',
+ # 'фамилия': 'Новодворская',
  # 'пол': 'женский',
- # 'фамилия': 'Леонова'}
-# >>>
-# >>> person = generate_person()
-# >>> pprint(person)
-# {'дата рождения': datetime.date(1951, 2, 9),
- # 'имя': 'Ливия',
- # 'мобильный': '+79775885447',
- # 'отчество': 'Ипполитовна',
+ # 'дата рождения': datetime.date(2019, 12, 14),
+ # 'мобильный': '+79192665326'}
+# {'имя': 'Алиса',
+ # 'отчество': 'Ильгизовна',
+ # 'фамилия': 'Макеева',
  # 'пол': 'женский',
- # 'фамилия': 'Злобина'}
-# >>>    
-    
-    
-    
-    
-
-
-    
-
+ # 'дата рождения': datetime.date(1973, 1, 4),
+ # 'мобильный': '+79111737099'}

dca288cb21626466790a193679f53df22e47581f
Pavel Grechishny <grechishny.p@mail.ru>
2023-10-10 17:46:17 +0300

    добавлено: атрибуты db_path, words

--- 2023.10.06/1.4.py
+++ 2023.10.06/1.4.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from sys import path
 from csv import reader, writer
 
 class CountableNouns:
@@ -10,8 +11,12 @@ class CountableNouns:
     :method save_words: запрашивает в stdin слова, добавляет в words и дописывает в файл.
     """
     
-    db_path: Path = None
-    words: dict[str, tuple[str, str]] = []
+    db_path: Path = Path(path[0]) / 'words.csv'
+    words: dict[str, tuple[str, str]] = {}
+    with open(db_path, encoding='utf-8') as filein:
+        file_reader = reader(filein, delimiter = ',')
+        words = {line[0]: tuple(line[1:]) for line in file_reader}
+    
     
     
     @classmethod
@@ -23,6 +28,9 @@ class CountableNouns:
         :return: согласованное с числом существительное. 
         """
         
+        print(cls.words[word])
+     
+     
     @classmethod
     def save_words(cls, word1: str = None) -> None:
         """Метод запрашивает в stdin слова, добавляет их в words и дописывает в файл."""
@@ -37,21 +45,7 @@ class CountableNouns:
 
 # ======================================================================================
 
-# with open("classmates.csv", encoding='utf-8') as r_file:
-    # # Создаем объект reader, указываем символ-разделитель ","
-    # file_reader = csv.reader(r_file, delimiter = ",")
-    # # Счетчик для подсчета количества строк и вывода заголовков столбцов
-    # count = 0
-    # # Считывание данных из CSV файла
-    # for row in file_reader:
-        # if count == 0:
-            # # Вывод строки, содержащей заголовки для столбцов
-            # print(f'Файл содержит столбцы: {", ".join(row)}')
-        # else:
-            # # Вывод строк
-            # print(f'    {row[0]} - {row[1]} и он родился в {row[2]} году.')
-        # count += 1
-    # print(f'Всего в файле {count} строк.')       
+       
 
 # with open("classmates.csv", mode="w", encoding='utf-8') as w_file:
     # file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")

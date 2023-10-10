4db149d05c1f7effb7adbdb9687d149d7d7bdd08
Pavel Grechishny <grechishny.p@mail.ru>
2023-10-10 20:00:12 +0300

    добавлено: методы save_words, pick

--- 2023.10.06/1.4.py
+++ 2023.10.06/1.4.py
@@ -18,6 +18,22 @@ class CountableNouns:
         words = {line[0]: tuple(line[1:]) for line in file_reader}
     
     
+    @classmethod
+    def save_words(cls, word1: str = None) -> None:
+        """Метод запрашивает в stdin слова, добавляет их в words и дописывает в файл.
+        
+        :param word1: слово добавляемое в словарь words
+        """
+        new_words = ['один', 'два', 'пять']
+        template = 'введите слово, согласующееся с числительным "{}": '
+        
+        if not word1:
+            new_words = [input(template.format(nw)) for nw in new_words]
+            cls.words[new_words[0]] = tuple(new_words[1:])
+        else:
+            new_words = [input(template.format(nw)) for nw in new_words[1:]]
+            cls.words[word1] = tuple(new_words)
+    
     
     @classmethod
     def pick(cls, number: int, word: str) -> str:
@@ -27,27 +43,25 @@ class CountableNouns:
         :param word: существительное для согласования.
         :return: согласованное с числом существительное. 
         """
+        if not word in cls.words:
+            cls.save_words(word)
         
-        print(cls.words[word])
-     
-     
-    @classmethod
-    def save_words(cls, word1: str = None) -> None:
-        """Метод запрашивает в stdin слова, добавляет их в words и дописывает в файл."""
+        choice_words = word, *cls.words[word]
+        last_digit = number % 10
+        decade_digit = number % 100 // 10
+        
+        if decade_digit == 1:
+            return choice_words[2]
+        if last_digit == 1:
+            return choice_words[0]
+        
+        if 1 < last_digit < 5:
+            return choice_words[1]
+        else:
+            return choice_words[2]
+            
+           
 
 
 
 
-
-
-
-
-
-# ======================================================================================
-
-       
-
-# with open("classmates.csv", mode="w", encoding='utf-8') as w_file:
-    # file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
-    # file_writer.writerow(["Имя", "Класс", "Возраст"])
-    # file_writer.writerow(["Женя", "3", "10"])    

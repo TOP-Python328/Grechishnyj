b5e35e0a05984d9a48d5c84b684d2f0e02470e4c
Pavel Grechishny <grechishny.p@mail.ru>
2023-10-10 12:17:04 +0300

    добавлено: ChessKing.__init__(self, color, square)

---
+++ 2023.10.06/1.3.py
@@ -0,0 +1,24 @@
+
+
+class ChessKing:
+    """Шахматная фигура короля
+    
+    :attr files: соответствие между буквой, обозначающей вертикаль шахмотной доски и числом:
+    :attr ranks: соответствие между строковым представлением числа (горизонталь) и числом:
+    """
+    files: dict[str, int] = {'abcdefgh'[i]: i+1 for i in range(8)}
+    ranks: dict[str, int] = {'12345678'[i]: i+1 for i in range(8)}
+    
+    
+    
+    def __init__(self, color: str = 'white', square: str = None):
+        """
+        :attr color: цвет фигуры
+        :attr square: поле шахмотной доски, где в данный момент находится фигура
+        """
+        self.color = color
+        self.square = {'white': 'e2', 'black': 'e8'}[color] if not square else square
+
+
+wk = ChessKing()
+bk = ChessKing('black')

7e294b0332604ee571b4ffbc43447a834554c1b7
Pavel Grechishny <grechishny.p@mail.ru>
2023-10-10 11:15:56 +0300

    опубликовано: решение 1.2.py

--- 2023.10.06/1.2.py
+++ 2023.10.06/1.2.py
@@ -5,7 +5,7 @@ from datetime import date, time, datetime
 # Переменные для аннотаций
 Charges = dict[date, float]
 
-class PowerMetr:
+class PowerMeter:
     """Описывает двухтарифный счётчик потребленной электрической мощности""" 
     
     def __init__(self):
@@ -19,7 +19,12 @@ class PowerMetr:
         self.tariff2: Number = 4.80
         self.tariff2_starts: time = time(7)
         self.tariff2_ends: time = time(23)
-        self.charges: Charges = {}
+        # словарь не пустой, для тестов
+        self.charges: Charges = {
+            date(2023, 7, 1): 219.75,
+            date(2023, 8, 1): 221.75,
+            date(2023, 9, 1): 111.75,
+        }
         
     
     def meter(self, power: Number) -> Decimal:
@@ -28,30 +33,58 @@ class PowerMetr:
         :param power: суммарная потребленная мощность
         :return charge: стоимость согласно тарифному плану
         """
-
         now = datetime.now()
         if self.tariff2_starts.hour <= now.time().hour < self.tariff2_ends.hour:
-            charge = power * self.tariff2
+            charge = Decimal(power * self.tariff2)
         else:
-            charge = power * self.tariff1
-
+            charge = Decimal(power * self.tariff1)
+    
         key = date(now.year, now.month, 1)
+        
         if key in self.charges:
             self.charges[key] += round(charge, 2)
         else:
             self.charges[key] = round(charge, 2)
-            
+        return Decimal(charge)
 
-        print(f'{self.charges=}')
-        return charge
+        
+    def __repr__(self):
+        name = type(self).__name__
+        power = sum(float(value) for value in self.charges.values())
+        return f'<{name}: {power} кВт/ч>'
+    
+    
+    def __str__(self):
+        str_charges = ''
+        for dt, power in self.charges.items():
+            str_charges += f'({dt.strftime("%B")[:3]}) {power}\n' 
+        return str_charges
 
-# >>> pm = PowerMetr()
+
+# >>> pm = PowerMeter()
+# >>> pm
+# <PowerMeter: 553.25 кВт/ч>
+# >>> pm.__str__()
+# '(Jul) 219.75\n(Aug) 221.75\n(Sep) 111.75\n'
+# >>> print(pm)
+# (Jul) 219.75
+# (Aug) 221.75
+# (Sep) 111.75
+
+# >>> pm.meter(4.5)
+# Decimal('21.599999999999997868371792719699442386627197265625')
+# >>> pm.__str__()
+# '(Jul) 219.75\n(Aug) 221.75\n(Sep) 111.75\n(Oct) 21.60\n'
 # >>> pm.meter(5)
-# self.charges={datetime.date(2023, 10, 1): 24.0}
-# 24.0
-# >>> pm.meter(5)
-# self.charges={datetime.date(2023, 10, 1): 48.0}
-# 24.0
-# >>> pm.meter(3.324)
-# self.charges={datetime.date(2023, 10, 1): 63.96}
-# 15.955199999999998
+# Decimal('24')
+# >>> pm.__str__()
+# '(Jul) 219.75\n(Aug) 221.75\n(Sep) 111.75\n(Oct) 45.60\n'
+# >>> print(pm)
+# (Jul) 219.75
+# (Aug) 221.75
+# (Sep) 111.75
+# (Oct) 45.60
+
+# >>> pm.__repr__()
+# '<PowerMeter: 598.85 кВт/ч>'
+# >>>

a8649427352de4584630dd8cc3cd789c6d857211
Pavel Grechishny <grechishny.p@mail.ru>
2023-10-13 11:19:44 +0300

    исправлено: 2023.08.18\1.py

--- 2023.08.18/1.py
+++ 2023.08.18/1.py
@@ -1,38 +1,31 @@
 def strong_password(password: str) -> bool:
-    """Функция возвращает результат проверки пароля."""
+    """Возвращает результат проверки пароля."""
     if len(password) < 8:
         return False
     # УДАЛИТЬ: else избыточен
-    else:
-        checkers = {
-            'lat_lower': 0,
-            'lat_upper': 0,
-            'punctuation': 0,
-            'digits': 0
-        }
+    checkers = {
+        'lat_lower': 0,
+        'lat_upper': 0,
+        'punctuation': 0,
+        'digits': -1
+    }
+    
     for char in password:
         symbol = ord(char)
         # ИСПОЛЬЗОВАТЬ: представьте числовую ось, поместите на неё метки 65 и 90, теперь нанесите на ось отметку symbol для истинного условия — в каком порядке у вас окажутся подписи меток, в таком же порядке записывайте неравенства — так станет куда проще воспринимать условие целиком
-        if 65 <= symbol and symbol <= 90:
+        if 65 <= symbol <= 90:
             checkers['lat_upper'] += 1
         # ИСПОЛЬЗОВАТЬ: также в Python возможно конструировать цепочки сравнительных операторов, таким образом and оказывается не нужен
         if 97 <= symbol <= 122:
             checkers['lat_lower'] += 1
         # ИСПРАВИТЬ: в большинстве случаев этого набора символов хватит, но не всегда — лучше смотреть есть ли любые другие символы помимо букв и цифр
-        if char in ' ,./<>?;\':\"-_=+()[]*&^%$#@':
+        if not char.isalpha() and not char.isdigit():
             checkers['punctuation'] += 1
         if char.isdigit():
             checkers['digits'] += 1
 
-    if checkers['digits'] < 2:
-        return False
-
     # ИСПРАВИТЬ: здесь имеет смысл воспользоваться встроенной функцией all()
-    for value in checkers.values():
-        if not value:
-            return False
-    else:
-        return True
+    return all(checkers.values())
 
 
 # >>> strong_password('aP3:kD_l3')

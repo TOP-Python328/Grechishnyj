929e27f776d8226ce8ca976bb7c560382487728a
Gennadiy <renardes@gmail.com>
2023-10-24 23:33:13 +0500

    2023.10.13: комментарии, правки, итоги

--- 2023.10.13/1.py
+++ 2023.10.13/1.py
@@ -4,21 +4,20 @@ from typing import Self, Callable
 from operator import add, sub, neg, mul
 from itertools import chain
 
+
 RawRow = Iterable[Number]
 RawMatrix = Iterable[RawRow]
 
+
 class Matrix:
     """Класс моделирует изменяемую матрицу"""
-    
-
     def __init__(self, raw_matrix: RawMatrix):
         """Конструктор экземпляра класса.
         
-        :attr raw_matrix: строки матрицы
-        :attr n: количество строк
-        :attr m: количество столбцов
+        :param raw_matrix: строки матрицы
         """
         if self.is_valid(raw_matrix):
+            # ИСПРАВИТЬ: здесь должен быть уже не любой итерируемый объект, а именно список списков чисел (см. тест ниже)
             self.__rows = raw_matrix
             self.n = len(raw_matrix)
             self.m = len(raw_matrix[0])
@@ -29,40 +28,42 @@ class Matrix:
     @staticmethod
     def is_valid(raw_matrix: RawMatrix) -> bool:
         """Метод проверяет, является ли аргумент подходящим объектом для конструирования матрицы"""
+        # ДОБАВИТЬ: обработчики исключений (см. тест ниже)
         if len(set(map(len, raw_matrix))) != 1:
-            return False   
+            return False
         for row in raw_matrix:
             if not iter(row):
                 return False
             for num in row:
                 if not isinstance(num, Number):
-                    return False            
+                    return False
         return True
 
- 
     @property
     def transpose(self: Self) -> Self:
         """Метод возвращает транспонированную матрицу"""
+        # ИСПРАВИТЬ: операция транспонирования должна возвращать новый объект, а не изменённый текущий экземпляр
         self.__rows = [[self.__rows[j][i] for j in range(self.n)] for i in range(self.m)]
         self.n, self.m = self.m, self.n
         return self
-
 
     def __getitem__(self: Self, index: int) -> RawRow:
         """Доступ на чтение строки (элемента) матрицы по индексу"""
         return self.__rows[index]
 
-    
     def __element_wise_operation(self, operation: Callable, other: Self | Number) -> Self:
         """Выполняет переданную операцию со своим и переданным объектом"""
+        # ИСПРАВИТЬ: предварительное создание нулевой матрицы избыточно, это лишние итерации — создавайте генераторными выражениями объект с вычисленными значениями и сразу передавайте этот объект в конструктор
         new_matrix = Matrix([[0 for _ in range(self.m)] for _ in range(self.n)])
-        if isinstance(other, Number): 
+        if isinstance(other, Number):
             for i in range(self.n):
                 for j in range(self.m):
                     new_matrix[i][j] = operation(self[i][j], other)
         elif isinstance(other, Matrix):
+            # ИСПРАВИТЬ: лучше сравнить напрямую идентичность с объектом функции
             if operation.__name__ == 'mul':
                 raise NotImplementedError('умножение матриц будет реализованно в будущем')
+            # ИСПРАВИТЬ: складываются матрицы только строго одной размерности, а не только по одному из измерений (см. тест ниже)
             if self.n == other.n or self.m == other.m:
                 for i in range(self.n):
                     for j in range(self.m):
@@ -70,45 +71,33 @@ class Matrix:
             else:
                 raise ValueError('сложение и вычитание возможно только для матриц одной размерности')
         return new_matrix
-        
-    
+
     def __add__(self, other) -> Self:
-        """docstring"""
         return self.__element_wise_operation(add, other)
 
-        
     def __radd__(self, other) -> Self:
-        """docstring"""
         return self.__element_wise_operation(add, other)
 
-      
     def __sub__(self, other) -> Self:
-        """docstring"""
-        return self.__element_wise_operation(sub, other)  
-
+        return self.__element_wise_operation(sub, other)
 
     def __rsub__(self, other) -> Self:
-        """docstring"""
-        return self.__element_wise_operation(sub, other) 
+        # ИСПРАВИТЬ: переставлять объекты в вычитании надо с сохранением знаков (см. тест ниже)
+        # КОММЕНТАРИЙ: я зачем вам унарное отрицание подкинул, мм?..)
+        return self.__element_wise_operation(sub, other)
 
-    
     def __mul__(self, other) -> Self:
-        """docstring"""
-        return self.__element_wise_operation(mul, other)  
-
+        return self.__element_wise_operation(mul, other)
 
     def __rmul__(self, other) -> Self:
-        """docstring"""
-        return self.__element_wise_operation(mul, other)  
-
+        return self.__element_wise_operation(mul, other)
 
     def __neg__(self) -> Self:
-        """docstring"""
-        return self.__element_wise_operation(mul, -1)       
-
+        return self.__element_wise_operation(mul, -1)
 
     # не выводит дробную часть
     # def __repr__(self):
+        # КОММЕНТАРИЙ: не выводит дробную часть, потому что %d — это код для целого числа, для вещественного — %f
         # format_item = f'%{len(str(max(abs(i) for i in chain(*self.__rows))))}d'
         # representation = ''
         # for i in range(self.n):
@@ -116,12 +105,13 @@ class Matrix:
         # return representation
 
     def __repr__(self):
+        # ИСПРАВИТЬ: используйте генераторное выражение в ещё одном join() — и не будет ни лишних пробелов, ни лишних eol
         representation = ''
         for i in range(self.n):
             representation += " ".join(f"{round(self[i][j], 1):>2}" for j in range(self.m)) + '\n'
         return representation
-        
-  
+
+
 # >>> a = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]
 # >>> m1 = Matrix(a)
 # >>> m1
@@ -136,6 +126,8 @@ class Matrix:
  # 1  2  3  4  5
  # 6  7  8  9 10
 # 11 12 13 14 15
+
+# ДОБАВИТЬ: тесты правосторонних методов перегрузки операторов (число плюс/минус матрица)
 
 # >>> m2
  # 3  6  9 12 15
@@ -185,4 +177,26 @@ class Matrix:
 # 18.0 40.5 63.0
 # 22.5 45.0 67.5
 
+# >>> m1 = Matrix(((1, 2), (3, 4)))
+# >>> m1[0][0] = -1
+# ...
+# TypeError: 'tuple' object does not support item assignment
 
+# >>> m1 = Matrix([1,2,3])
+# ...
+# TypeError: object of type 'int' has no len()
+
+# >>> m1 = Matrix([(1, 2), (3, 4)])
+# >>> m2 = Matrix([(1, 2, 3, 4), (5, 6, 7, 8)])
+# >>> m2 + m1
+# ...
+# IndexError: tuple index out of range
+
+# >>> m1 = Matrix([(1, 1), (1, 1)])
+# >>> 5 - m1
+# -4 -4
+# -4 -4
+# КОММЕНТАРИЙ: а должны быть положительными
+
+
+# ИТОГ: хорошо — 7/10

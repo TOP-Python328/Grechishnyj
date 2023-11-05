5b1b2d0ec8c4137f9b602d75da7ace06656c866e
Pavel Grechishny <grechishny.p@mail.ru>
2023-11-05 13:18:17 +0300

    добавлено: частичное решение 1.4.py

--- 2023.09.08/1.4.py
+++ 2023.09.08/1.4.py
@@ -1,33 +1,48 @@
 from urllib.request import urlopen, urlparse
+from posixpath import basename
+from json import loads as json_loads
+
 from os.path import splitext
 from pathlib import Path
+from sys import path
+from re import findall, S
 
 url = f'https://www.python.org'
 url2 = f'https://docs.python.org/3/py-modindex.html'
 # url3 = f'https://www.world-art.ru/cinema/rating_top.php'
 
 
-# html = urlopen(URL).read().decode('utf-8')
-# html = html.decode('utf-8')
-
 def json_from_html(url: str, template: str, encod: str='utf-8') -> Path:
     """Функция по заданному шаблону извлекает из HTML документа данные и помещает их в JSON файл"""
     html = urlopen(url).read().decode(encod)
-    
-    parsed = urlparse(url)
-    print(parsed)
-    # root, ext = splitext(parsed.path)
+    url_name = basename(urlparse(url).path)
     
     
-    print(url)
-    # print(file_name)
+    html_data = findall(template, html, S)
+     
+    html_text = '{\n' + ''.join(f'\t"{line[0]}": "{line[1]}",\n' for line in html_data) + '\n}'
+
     
-    json_path = ''
+    if url_name.endswith('.html'):
+        json_name = url_name.replace('.html', '.json')
+    else:
+        json_name = f'{splitext(__loader__.path)[0]}.json' 
+
+    json_path = Path(path[0]) / json_name
+    
+    
+    with open(json_path, 'w', encoding='utf-8') as file_out:
+        file_out.write(html_text)
+        
+    
     
     return json_path
     
-json_from_html(url, 'd')
-json_from_html(url2, 'd')
-# json_from_html(url3, 'd')
+films_pattern = (r'<tr .*?>'
+                    r'<td .*?<a.*?>(?P<name>.*?)</a>.*?</td>'
+                    r'<td .*?>(?P<rating>.*?)</td>')
 
+modules_pattern = r'<tr>.+?>(\w+?)<.+?</td><td>.*?<em>(.*?)</em>'
 
+json_from_html(url2, modules_pattern)
+json_from_html(url, films_pattern)

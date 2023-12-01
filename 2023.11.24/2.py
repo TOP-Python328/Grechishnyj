4e1c12a9b20587b07853e4ec936bc7beb09e39a3
Pavel Grechishny <grechishny.p@mail.ru>
2023-12-01 17:48:31 +0300

    добавлено: **kwargs 2.py

--- 2023.11.24/2.py
+++ 2023.11.24/2.py
@@ -12,9 +12,10 @@ class HTMLTag:
     """
     default_indent_spaces: int = 2
     
-    def __init__(self, name: str, value: str = ''):
+    def __init__(self, name: str, value: str = '', **kw_attrs):
         self.name = name
         self.value = value
+        self.attrs: dict = {str(key): str(value) for key, value in kw_attrs.items()}
         self.__nested: list[HTMLTag] = []
     
     def nested(self, html_tag: Self):
@@ -25,7 +26,8 @@ class HTMLTag:
         """Рекурсивно формирует строку с текущим и всеми вложенными тегами."""
         margin = ' ' * indent_level * self.default_indent_spaces
         eol = ''
-        result = f"{margin}<{self.name}>{self.value}"
+        attrs = ' '.join(['='.join((f"{key}", f'"{value}"')) for key, value in self.attrs.items()])
+        result = f"{margin}<{self.name} {attrs}>{self.value}"
         if self.__nested:
             for tag in self.__nested:
                 result += '\n' + tag.__str(indent_level+1)
@@ -56,15 +58,15 @@ class HTMLBuilder:
         self.root: HTMLTag = root
         self.__parent: Self = parent
     
-    def nested(self, name: str, value: str = '') -> Self:
+    def nested(self, name: str, value: str = '', **kw_attrs) -> Self:
         """Добавляет вложенный тег к текущему тегу и возвращает строитель для вложенного тега."""
-        tag = HTMLTag(name, value)
+        tag = HTMLTag(name, value, **kw_attrs)
         self.root.nested(tag)
         return HTMLBuilder(tag, parent=self)
     
-    def sibling(self, name: str, value: str = '') -> Self:
+    def sibling(self, name: str, value: str = '', **kw_attrs) -> Self:
         """Добавляет вложенный тег к текущему тегу и возвращает текущий строитель."""
-        tag = HTMLTag(name, value)
+        tag = HTMLTag(name, value, **kw_attrs)
         self.root.nested(tag)
         return self
 
@@ -86,37 +88,67 @@ def write_html_file(html: HTMLTag, fileout: str = '2') -> None:
     (Path(path[0]) / f'{fileout}.html').write_text(str(html), encoding='utf-8')
 
 # >>> html = HTMLTag.create('html')\
-# ... .nested('head')\
-# ... .sibling('title')\
-# ... .sibling('script')\
-# ... .sibling('style')\
-# ... .closest()\
-# ... .nested('body')\
-# ... .nested('header')\
-# ... .nested('div', 'Menu')\
+# ... .nested('body', 'Main', bg='yellow', font='sans')\
+# ... .nested('header', bg='green', fontsize='20px')\
+# ... .nested('div', border='1px sold white')\
 # ... .nested('ul')\
-# ... .sibling('li')\
+# ... .sibling('li', 'Pos_1', color='grey')\
+# ... .sibling('li', 'Pos_2', color='blue')\
+# ... .sibling('li', 'Pos_3', color='grey')\
 # ... .closest()\
-# ... .nested('main')\
+# ... .nested('ul')\
+# ... .sibling('li', 'Pos_4', color='grey')\
+# ... .sibling('li', 'Pos_5', color='grey')\
+# ... .sibling('li', 'Pos_6', color='blue')\
+# ... .closest()\
+# ... .closest()\
+# ... .nested('main', class_='itembox')\
+# ... .nested('div', class_='item', color='#f1f2f3')\
+# ... .nested('ul')\
+# ... .sibling('li', 'Pos_4', color='grey')\
+# ... .sibling('li', 'Pos_5', color='grey')\
+# ... .sibling('li', 'Pos_6', color='blue')\
+# ... .closest()\
+# ... .nested('div', class_='item', color='#f1f2f4')\
+# ... .nested('ul')\
+# ... .sibling('li', 'Pos_4', color='grey')\
+# ... .sibling('li', 'Pos_5', color='grey')\
+# ... .sibling('li', 'Pos_6', color='blue')\
 # ... .build()
 # >>>
-# >>> write_html_file(html)
-# >>>
 # >>> print(html)
-# <html>
-  # <head>
-    # <title></title>
-    # <script></script>
-    # <style></style>
-  # </head>
-  # <body>
-    # <header>
-      # <div>Menu
-        # <ul>
-          # <li></li>
+# <html >
+  # <body bg="yellow" font="sans">Main
+    # <header bg="green" fontsize="20px">
+      # <div border="1px sold white">
+        # <ul >
+          # <li color="grey">Pos_1</li>
+          # <li color="blue">Pos_2</li>
+          # <li color="grey">Pos_3</li>
         # </ul>
-        # <main></main>
+        # <ul >
+          # <li color="grey">Pos_4</li>
+          # <li color="grey">Pos_5</li>
+          # <li color="blue">Pos_6</li>
+        # </ul>
       # </div>
+      # <main class_="itembox">
+        # <div class_="item" color="#f1f2f3">
+          # <ul >
+            # <li color="grey">Pos_4</li>
+            # <li color="grey">Pos_5</li>
+            # <li color="blue">Pos_6</li>
+          # </ul>
+          # <div class_="item" color="#f1f2f4">
+            # <ul >
+              # <li color="grey">Pos_4</li>
+              # <li color="grey">Pos_5</li>
+              # <li color="blue">Pos_6</li>
+            # </ul>
+          # </div>
+        # </div>
+      # </main>
     # </header>
   # </body>
 # </html>
+# >>>
